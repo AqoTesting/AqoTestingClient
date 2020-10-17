3fadd55ca81dba8c401359f95ae0a18224093778
Jebati <www.den-safonov@mail.ru>
2020-10-17 14:02:24 +0300

    Add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from "./modules/material.module";
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 const MaterialComponents = [
-  MatButtonModule
+  MatButtonModule,
+  MatCardModule,
+  MatToolbarModule,
+  MatIconModule,
 ];
 
 @NgModule({
-  imports: [
-    MaterialComponents
-  ],
-  exports: [
-    MaterialComponents
-  ]
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
 })
-export class MaterialModule { }
+export class MaterialModule {}
