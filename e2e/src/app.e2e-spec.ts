9aa88858b3ae78f496f558701551540933119be7
Jebati <www.den-safonov@mail.ru>
2020-12-14 03:57:01 +0300

    AqoTestingClient to \AqoTestingDomainClient

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('AqoTestingClient app is running!');
+    expect(page.getTitleText()).toEqual('AqoTestingDomainClient app is running!');
   });
 
   afterEach(async () => {
